4aaa77b59fa927eebea9d05430599f1d9c8d8507
Joao-Pedro-J <matheusitalo125@gmail.com>
2026-02-17 13:48:33 -0300

    feat: create window to manipulate actions of `Replicaçao - Processo e Tabela`

--- src/main/java/com/jpdev/desafio_bradesco/database/connection/ControleConnection.java
+++ src/main/java/com/jpdev/desafio_bradesco/database/connection/ControleConnection.java
@@ -7,7 +7,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.Properties;
 
-public class ControlConnection implements DbConnection{
+public class ControleConnection implements DbConnection{
     @Override
     public Connection getConnection() throws SQLException {
         Properties env = EnvLoader.load();
