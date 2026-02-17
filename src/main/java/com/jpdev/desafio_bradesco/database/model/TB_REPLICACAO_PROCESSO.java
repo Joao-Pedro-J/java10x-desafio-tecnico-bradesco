21b28388c5ff0fedeadf88148894e4f60ad244a4
Joao-Pedro-J <matheusitalo125@gmail.com>
2026-02-16 23:04:58 -0300

    feat: create model of table `TB_REPLICACAO_PROCESSO`

---
+++ src/main/java/com/jpdev/desafio_bradesco/database/model/TB_REPLICACAO_PROCESSO.java
@@ -0,0 +1,59 @@
+package com.jpdev.desafio_bradesco.database.model;
+
+public class TB_REPLICACAO_PROCESSO {
+    private Long id;
+    private String processo;
+    private String descricao;
+    private Boolean habilitado;
+
+    public TB_REPLICACAO_PROCESSO() {
+    }
+
+    public TB_REPLICACAO_PROCESSO(String processo, String descricao, Boolean habilitado) {
+        this.processo = processo;
+        this.descricao = descricao;
+        this.habilitado = habilitado;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getProcesso() {
+        return processo;
+    }
+
+    public void setProcesso(String processo) {
+        this.processo = processo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public Boolean getHabilitado() {
+        return habilitado;
+    }
+
+    public void setHabilitado(Boolean habilitado) {
+        this.habilitado = habilitado;
+    }
+
+    @Override
+    public String toString() {
+        return "TB_REPLICAO_PROCESSO{" +
+                "id=" + id +
+                ", processo='" + processo + '\'' +
+                ", descricao='" + descricao + '\'' +
+                ", habilitado=" + habilitado +
+                '}';
+    }
+}
